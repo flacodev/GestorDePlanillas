46cc316d970c8e72909e4843a1725f37b092c8a3
flacodev <agustin7tintin@gmail.com>
2025-08-21 20:31:51 -0300

    Version 0.0.1 agregado de clase planilla incompleta

---
+++ src/main/java/org/example/Planilla.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.ArrayList;
+public class Planilla {
+    private int ID;
+    private String Nombre;
+    private List<Planilla> Planillas = Arrays.asList();
+    private int CantDeLavados;
+    private float cantidadCloro;
+    private String pintura;
+
+
+}
